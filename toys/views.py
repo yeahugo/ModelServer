cdf3192e0487ed6a09c32d429d6894fd8b91d899
yeahugo <yjhuestc@gmail.com>
2014-12-10 23:53:37 +0800

    使用mongodb 读取数据

--- toys/views.py
+++ toys/views.py
@@ -5,6 +5,8 @@ from django.core import serializers
 
 # Create your views here.
 def index(request):
-    data = serializers.serialize("json",Toy.objects.all())
+    print Toy.objects.all()
+#    data = serializers.serialize("json",Toy.objects.all())
+    data = Toy.objects.to_json()
     print data
     return HttpResponse(data)
